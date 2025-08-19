781da8d2a5172d6f7a34d6523ca376994f15e7de
Shahzaib Waseem <shahzaib.waseem27@gmail.com>
2025-08-19 14:20:05 +0200

    task1 working

---
+++ task_1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'task_1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shazzy',
+    maintainer_email='shahzaib.waseem27@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'task_1 = task_1.task_1:main'
+        ],
+    },
+)
